72a7b8b32c9cc9e924bf3cde26dbe461b16ee130
arche123456 <2391922388@qq.com>
2025-11-07 07:39:37 -0800

    话题通信完成

--- src/cpp01_topic/src/demo03_talker_stu.cpp
+++ src/cpp01_topic/src/demo03_talker_stu.cpp
@@ -6,7 +6,7 @@
     3.自定义节点类
       3.1创建消息发布方
       3.2创建定时器
-      3.2组织并发布消息
+      3.3组织并发布消息
     4.调用spin函数，传入自定义类对象指针
     5.释放资源
 */
@@ -14,11 +14,31 @@
 #include "rclcpp/rclcpp.hpp"
 #include "base_interface_demo/msg/student.hpp"
 
+using base_interface_demo::msg::Student;
+using namespace std::chrono_literals;
 class TalkerStu: public rclcpp::Node{
     public:
-        TalkerStu():Node("talkerstu_node_cpp"){
-            RCLCPP_INFO(this->get_logger(),"talkerstu_node_cpp节点创建成功");
+        TalkerStu():Node("talkerstu_node_cpp"), age(0){
+        // 3.1创建消息发布方
+        publisher_ = this->create_publisher<Student>("chatter_stu", 10);
+        // 3.2创建定时器
+        timer_ = this->create_wall_timer(500ms, std::bind(&TalkerStu::on_timer, this));
+        RCLCPP_INFO(this->get_logger(),"talkerstu_node_cpp节点创建成功");
         }
+    private:
+        void on_timer(){
+        // 3.3组织并发布消息
+        auto stu = Student();
+        stu.name = "huluwa";
+        stu.age = age;
+        stu.height = 2.20;
+        age++;
+        publisher_ ->publish(stu);
+        RCLCPP_INFO(this->get_logger(),"发布的消息:(%s, %d, %.2f)", stu.name.c_str(), stu.age, stu.height);
+        }
+        rclcpp::Publisher<Student>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        int age;
 };
 
 int main(int argc, char ** argv)
--- src/cpp01_topic/src/demo04_listener_stu.cpp
+++ src/cpp01_topic/src/demo04_listener_stu.cpp
@@ -1,24 +1,33 @@
 /*
-  需求：以某个固定频率发送文本“hello world！”，文本后缀编号，每发布一条，编号+1
+  需求：订阅学生需求并打印在终端
   流程：
     1.包含头文件
     2.初始化ROS客户端
     3.自定义节点类
-      3.1创建消息发布方
-      3.2创建定时器
-      3.2组织并发布消息
+      3.1创建消息订阅方
+      3.2回调函数订阅并解析数据
     4.调用spin函数，传入自定义类对象指针
     5.释放资源
 */
 //1.包含头文件
 #include "rclcpp/rclcpp.hpp"
 #include "base_interface_demo/msg/student.hpp"
-
+using base_interface_demo::msg::Student;
 class ListenerStu: public rclcpp::Node{
     public:
         ListenerStu():Node("listenerstu_node_cpp"){
-            RCLCPP_INFO(this->get_logger(),"listenerstu_node_cpp节点创建成功");
+          // 3-1创建订阅方
+          //模板为订阅消息类型，参数1为话题名称，参数2为队列长度，参数3为当前类下的回调函数，参数4为调用对象为当前对象，参数5为回调函数参数为所占字节
+          subscription_ = this->create_subscription<Student>("chatter_stu", 10, std::bind(&ListenerStu::do_back, this, std::placeholders::_1));
+          RCLCPP_INFO(this->get_logger(),"listenerstu_node_cpp节点创建成功");
         }
+    private:
+        void do_back(const Student::SharedPtr stu){
+          //3-2回调函数订阅并解析数据
+          RCLCPP_INFO(this->get_logger(), "订阅的学生信息:name=%s, age=%d, height=%.2f", stu->name.c_str(), stu->age, stu->height);
+
+        }
+        rclcpp::Subscription<Student>::SharedPtr subscription_;
 };
 
 int main(int argc, char ** argv)
