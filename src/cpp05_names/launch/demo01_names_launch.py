a084ac8bc04a6475a590c5490673d87a4548968b
arche123456 <2391922388@qq.com>
2025-11-16 10:01:51 -0800

    完成节点话题重名问题

--- src/cpp05_names/launch/demo01_names_launch.py
+++ src/cpp05_names/launch/demo01_names_launch.py
@@ -3,6 +3,17 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+#     #修改节点名称
+#     return LaunchDescription([
+#         Node(package = 'turtlesim', executable ="turtlesim_node", name = "turtle1"),
+#         Node(package = 'turtlesim', executable ="turtlesim_node", namespace = "t1"),
+#         Node(package = 'turtlesim', executable ="turtlesim_node", namespace = "t1", name = "turtle1")
+
+#     ])
+    #修改话题名称
     return LaunchDescription([
-        Node(package = 'turtlesim', executable ="turtlesim_node", name ='t1')
+        Node(package = "turtlesim", executable = "turtlesim_node", name = "my_turtlesim"),
+        Node(package = "turtlesim", executable = "turtlesim_node", 
+             remappings = [("/turtle1/cmd_vel", "/cmd_vel")]
+             )
     ])
--- src/cpp05_names/src/demo01_names.cpp
+++ src/cpp05_names/src/demo01_names.cpp
@@ -1,10 +1,26 @@
-#include <cstdio>
+//1.包含头文件
+#include "rclcpp/rclcpp.hpp"
+
+//3.自定义节点类
+class MyNode: public rclcpp::Node
+{
+  public:
+    MyNode():Node("mynode_node_cpp", "t1_ns")
+    {
+      RCLCPP_INFO(this->get_logger(),"mynode_node_cpp节点创建成功！");
+
+    }
+  private:
+};
+
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world cpp05_names package\n");
+  //2.初始化ROS客户端
+  rclcpp::init(argc, argv);
+  //4.调用spin函数，传入自定义类对象指针
+  rclcpp::spin(std::make_shared<MyNode>());//持续处理传入节点的所有回调
+  //5，释放资源
+  rclcpp::shutdown();
   return 0;
 }
