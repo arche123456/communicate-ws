5ad6e3ee67752693b694b2692c568afa09ac8a2f
arche123456 <2391922388@qq.com>
2025-11-19 08:08:51 -0800

    镜像乌龟完成

--- src/cpp07_exercise/src/exer01_pub_sub.cpp
+++ src/cpp07_exercise/src/exer01_pub_sub.cpp
@@ -1,17 +1,44 @@
 /*
-  需求：以某个固定频率发送文本“hello world！”，文本后缀编号，每发布一条，编号+1
+  需求：订阅乌龟1的位姿信息，解析出线速度和角速度，生成并发布控制乌龟2运动的速度指令
+  明确：
+    订阅话题：/turtle1/pose
+    订阅消息：turtlesim/msg/Pose
+        x: 0.0
+        y: 0.0
+        theta: 0.0
+        linear_velocity: 0.0
+        angular_velocity: 0.0
+    发布话题：t2/turtle2/cmd_vel
+    发布消息：geometry_msgs/msg/Twist
+        float32 linear 线速度
+            float32 x  前后方向运动速度
+            float32 y  左右方向运动速度
+            float32 z  上下方向运动速度
+        float32 angular角速度
+            float32 x  roll轴旋转速度
+            float32 y  pitch轴旋转速度
+            float32 z  yaw轴旋转速度
   流程：
     1.包含头文件
     2.初始化ROS客户端
     3.自定义节点类
-      3.1创建消息发布方
-      3.2创建定时器
-      3.2组织并发布消息
+      3.1创建发布方
+      3.2创建订阅方（订阅乌龟1的位姿信息，解析速度）
+      3.3订阅方的回调函数，处理速度，生成并发布控制乌龟2运动的速度指令
     4.调用spin函数，传入自定义类对象指针
     5.释放资源
+    BUG描述：
+      乌龟1后退时，乌龟2仍前进
+    BUG原因：
+    1.和乌龟位姿发布有关，当乌龟实际速度为负数时，位姿中的速度仍是正数
+    2.发布的乌龟2的速度，与位姿中的线速度一致 
+    BUG修复：
+      修改源码，将位姿中的线速度计算修改为直接等于x方向速度
 */
 //1.包含头文件
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"  //发布话题
+#include "turtlesim/msg/pose.hpp"       //订阅话题
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;//使用时间命名空间，可以数字加单位
@@ -22,36 +49,26 @@ class Exer01PubSub: public rclcpp::Node
     Exer01PubSub():Node("exer01_pub_sub_node_cpp")
     {
       RCLCPP_INFO(this->get_logger(),"案例1对象创建");
-      //3-1创建消息发布方
-      /*
-        模板：被发布的消息类型
-        参数：
-          1.话题名称
-          2.消息队列长度
-        返回值：发布对象指针
-      */
-      publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);//创建了一个发布方，发布到话题chatter上，队列长度为10
-      //3-2创建定时器
-      /*
-        参数：
-          1.时间间隔
-          2.回调函数
-        返回值：定时器对象指针
-      */
-      timer_ = this->create_wall_timer(1s, std::bind(&Exer01PubSub::on_timer, this));
+      // 3.1创建发布方
+      pub_ = this->create_publisher<geometry_msgs::msg::Twist>("t2/turtle1/cmd_vel", 10);
+      sub_ = this->create_subscription<turtlesim::msg::Pose>("/turtle1/pose",10,
+        std::bind(&Exer01PubSub::pose_cb, this, std::placeholders::_1));
+      // 3.2创建订阅方（订阅乌龟1的位姿信息，解析速度）
+
     }
   private:
-    void on_timer()
+    void pose_cb(turtlesim::msg::Pose::SharedPtr pose)
     {
-      //3-3组织并发布消息
-      auto message = std_msgs::msg::String();
-      message.data = "hello world! " + std::to_string(count++);
-      RCLCPP_INFO(this->get_logger(), "发布方发布的消息：%s", message.data.c_str());//将std::string字符串转换为C风格字符串const char*
-      publisher_->publish(message);
+        // 3.3订阅方的回调函数，处理速度，生成并发布控制乌龟2运动的速度指令
+        //创建速度指令
+        geometry_msgs::msg::Twist twist;
+        twist.linear.x = pose->linear_velocity; //线速度
+        twist.angular.z = pose->angular_velocity; //角速度
+        //发布
+        pub_->publish(twist);
     }
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;//创建了一个发布方指针用来接收发布的对象
-    rclcpp::TimerBase::SharedPtr timer_;//创建定时器指针
-    size_t count;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr sub_;
   };
 
 
